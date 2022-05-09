35bc42b6400f87fbf82ebe644686f2a843fdd956
mightygoat <alpha5and5official@gmail.com>
2022-05-09 07:01:48 +0530

    Initial Physics Engine

---
+++ Duniya/src/PhysicsSystem.cpp
@@ -0,0 +1,19 @@
+#include <PhysicsSystem.hpp>
+
+PhysicsSystem::PhysicsSystem()
+{
+	collisionBoxes.resize(1000);
+}
+
+void PhysicsSystem::LoadScene(Scene* scene)
+{
+	for(auto& entities: scene->entities)
+	{
+		auto collisionBox3D = entities->Get<CollisionBox3D>(ComponentTypes::COLLISIONBOX3D);
+		if(collisionBox3D != nullptr)
+		{
+		}
+	}
+}
+
+
---
+++ Duniya/src/PhysicsSystem.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#include <ECS/ECS.hpp>
+#include <vector>
+
+struct RigidBody
+{
+	float mass;
+};
+
+struct CollisionBox2D
+{
+	float x, y, length, width;
+};
+
+struct CollisionBox3D
+{
+	float x, y, z, length,  width, height;
+};
+
+namespace ComponentTypes
+{
+	enum : uint32_t
+	{
+		RIGIDBODY = 93,
+		COLLISIONBOX2D,
+		COLLISIONBOX3D
+	};
+};
+
+class PhysicsSystem : public System
+{
+public:
+	PhysicsSystem();
+	~PhysicsSystem() = default;
+	void LoadScene(Scene* scene) override;
+	void Update(float deltaTime) override;
+private:
+	std::vector<std::list<Scene::Entities>> collisionBoxes;
+};
